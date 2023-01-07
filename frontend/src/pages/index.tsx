f6bc6735355b439af04a6100997905c94a28b295
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2023-01-07 10:03:22 +0900

    ログイン機能の見た目部分を作成

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,49 @@
 import { Button } from "@mui/material";
 import { NextPage } from "next";
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 
 const Home: NextPage = () => {
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log({
+  //     email,
+  //     password,
+  //   });
+  // }
+
+  // const handleChangeEmail = (e) => {
+  //   setEmail(e.target.value);
+  // }
+
+  // const handleChangePassword = (e) => {
+  //   setPassword(e.target.value);
+  // }
+
   return (
-      <Button variant="contained" color="warning">
-        Hello World
-      </Button>
+    <div>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id='email' {...register('email')}/>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input id='password' {...register('password')}/>
+        </div>
+        <div>
+          <button type="submit">送信</button>
+        </div>
+      </form>
+
+    </div>
   );
 };
 
